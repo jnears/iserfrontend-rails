821a811d9cad856b43a8d7b3956659a52b964eb5
Paul Groves <github@modagoo.co.uk>
2013-09-25 16:54:40 +0100

    jquery ui sortable

--- app/assets/javascripts/iserwww.js
+++ app/assets/javascripts/iserwww.js
@@ -2,4 +2,5 @@
 //= require vendor/jquery-2.0.3.min
 //= require_directory ./vendor/bootstrap
 //= require vendor/bootstrap-datetimepicker.min
+//= require vendor/jquery-ui-1.10.3.custom.min
 //= require iserfrontend
